c9445d4820ef252f681e8f1f276e3ccdc5faef11
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2015-06-12 21:17:06 +0200

    movie details update

--- app/src/main/java/matvidako/hr/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/matvidako/hr/popularmovies/MovieDetailsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -19,6 +20,12 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
     @InjectView(R.id.image)
     ImageView image;
+    @InjectView(R.id.release_date)
+    TextView tvReleaseDate;
+    @InjectView(R.id.plot)
+    TextView tvPlot;
+    @InjectView(R.id.rating)
+    TextView tvRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +57,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
         ButterKnife.inject(this);
         getSupportActionBar().setTitle(movie.original_title);
         Picasso.with(this).load(Movie.Tools.getFullPosterPathLarge(movie)).into(image);
+        tvPlot.setText(movie.overview);
+        tvRating.setText("" + movie.vote_average);
+        tvReleaseDate.setText(movie.release_date.toString());
     }
 
 }
