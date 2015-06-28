9c75c4294241508d9599762dae75afd9bc404851
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2015-06-28 18:36:58 +0200

    added toolbar to settings activity

--- app/src/main/java/matvidako/hr/popularmovies/settings/SettingsActivity.java
+++ app/src/main/java/matvidako/hr/popularmovies/settings/SettingsActivity.java
@@ -2,6 +2,10 @@ package matvidako.hr.popularmovies.settings;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import matvidako.hr.popularmovies.R;
 
@@ -11,6 +15,21 @@ public class SettingsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0);
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
